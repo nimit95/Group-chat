94790a43581dd1469a21f2d7d867072e80dcf125
piyush6348 <piyush6348@gmail.com>
2017-08-05 01:42:02 +0530

    message firebase

---
+++ app/src/main/java/com/codingblocks/groupchat/FirebaseReference.java
@@ -0,0 +1,16 @@
+package com.codingblocks.groupchat;
+
+import android.app.Application;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by piyush on 4/8/17.
+ */
+
+public class FirebaseReference extends Application {
+    public static FirebaseDatabase database = FirebaseDatabase.getInstance();
+    public static DatabaseReference userReference = database.getReference("users");
+    public static DatabaseReference groupsReference = database.getReference("groups");
+}
---
+++ app/src/main/java/com/codingblocks/groupchat/token/ObtainInstanceID.java
@@ -0,0 +1,24 @@
+package com.codingblocks.groupchat.token;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by piyush on 4/8/17.
+ */
+
+public class ObtainInstanceID extends FirebaseInstanceIdService {
+    public static String TAG="";
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+
+        TAG = this.getClass().getSimpleName();
+
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.e(TAG, "onTokenRefresh: " + refreshedToken );
+
+    }
+}
