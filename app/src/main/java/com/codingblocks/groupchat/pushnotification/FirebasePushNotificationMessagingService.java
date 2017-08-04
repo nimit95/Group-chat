d0a9fde9e4816fc3856f571ad0b2d3bd6c6232e0
piyush6348 <piyush6348@gmail.com>
2017-08-05 02:22:39 +0530

    bugs fixed

--- app/src/main/java/com/codingblocks/groupchat/pushnotification/FirebasePushNotificationMessagingService.java
+++ app/src/main/java/com/codingblocks/groupchat/pushnotification/FirebasePushNotificationMessagingService.java
@@ -1,15 +1,15 @@
-//package com.codingblocks.groupchat.pushnotification;
-//
-//import com.google.firebase.messaging.FirebaseMessagingService;
-//import com.google.firebase.messaging.RemoteMessage;
-//
-///**
-// * Created by piyush on 4/8/17.
-// */
-//
-//public class FirebasePushNotificationMessagingService extends FirebaseMessagingService {
-//    @Override
-//    public void onMessageReceived(RemoteMessage remoteMessage) {
-//        super.onMessageReceived(remoteMessage);
-//    }
-//}
+package com.codingblocks.groupchat.pushnotification;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by piyush on 4/8/17.
+ */
+
+public class FirebasePushNotificationMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+}
--- app/src/main/java/com/codingblocks/groupchat/token/ObtainInstanceID.java
+++ app/src/main/java/com/codingblocks/groupchat/token/ObtainInstanceID.java
@@ -1,24 +1,26 @@
-//package com.codingblocks.groupchat.token;
-//
-//import android.util.Log;
-//
-//import com.google.firebase.iid.FirebaseInstanceId;
-//import com.google.firebase.iid.FirebaseInstanceIdService;
-//
-///**
-// * Created by piyush on 4/8/17.
-// */
-//
-//public class ObtainInstanceID extends FirebaseInstanceIdService {
-//    public static String TAG="";
-//    @Override
-//    public void onTokenRefresh() {
-//        super.onTokenRefresh();
-//
-//        TAG = this.getClass().getSimpleName();
-//
-//        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-//        Log.e(TAG, "onTokenRefresh: " + refreshedToken );
-//
-//    }
-//}
+package com.codingblocks.groupchat.token;
+
+import android.util.Log;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by piyush on 4/8/17.
+ */
+
+public class ObtainInstanceID extends FirebaseInstanceIdService {
+    public static String TAG="";
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+
+        TAG = this.getClass().getSimpleName();
+
+        FirebaseApp.initializeApp(getApplicationContext());
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.e(TAG, "onTokenRefresh: " + refreshedToken );
+
+    }
+}
