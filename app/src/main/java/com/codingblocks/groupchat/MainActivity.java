243f044dcfaa8dc99d1f0572a21f156427c022fc
Nimit Aggarwal <nimitagg95@gmail.com>
2017-08-04 16:46:02 +0530

    firebase add

--- app/src/main/java/com/codingblocks/groupchat/MainActivity.java
+++ app/src/main/java/com/codingblocks/groupchat/MainActivity.java
@@ -3,11 +3,18 @@ package com.codingblocks.groupchat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.SignInButton;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+
+
     }
 }
