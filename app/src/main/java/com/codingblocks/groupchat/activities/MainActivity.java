17b8c0840c079cfdfb81dcf0d47f158fe2c72554
Nimit Aggarwal <nimitagg95@gmail.com>
2017-08-04 17:47:20 +0530

    model class add

--- app/src/main/java/com/codingblocks/groupchat/activities/MainActivity.java
+++ app/src/main/java/com/codingblocks/groupchat/activities/MainActivity.java
@@ -2,14 +2,36 @@ package com.codingblocks.groupchat.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.codingblocks.groupchat.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button createNewGroup, joinGroup;
+
+        createNewGroup = (Button) findViewById(R.id.create_new_group);
+        joinGroup = (Button) findViewById(R.id.join_group);
+
+        createNewGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        joinGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/codingblocks/groupchat/model/models.kt
+++ app/src/main/java/com/codingblocks/groupchat/model/models.kt
@@ -3,3 +3,8 @@ package com.codingblocks.groupchat.model
 /**
  * Created by nimit on 4/8/17.
  */
+
+
+data class Group(val groupName: String,
+                  val groupId: String,
+                 val messages:String )
